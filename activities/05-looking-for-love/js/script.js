83e824409829bc7c70ac95cecadf37a4e19b40f6
Julie <gulshan.khashimova@gmail.com>
2021-10-03 14:38:59 -0400

    A5: refactored code into functions

--- activities/05-looking-for-love/js/script.js
+++ activities/05-looking-for-love/js/script.js
@@ -29,41 +29,48 @@ function setup(){
   circle1.vy = random(-circle1.speed, circle1.speed);
   circle2.vx = random(-circle2.speed, circle2.speed);
   circle2.vy = random(-circle2.speed, circle2.speed);
-
-
 }
 
 function draw(){
   background(0);
 
-  // makes the circles move
+  move();
+  checkOffscreen();
+  checkOverlap();
+  display();
+  //simulation();
+}
+
+  // moves circles
+  function move(){
   circle1.x = circle1.x + circle1.vx;
   circle1.y = circle1.y + circle1.vy;
 
   circle2.x = circle2.x + circle2.vx;
   circle2.y = circle2.y + circle2.vy;
-
+}
 
   //checks if circles go off screen
-  if circle1.x < 0 || circle1.x > width || circle1.y < 0 || circle1.y > height || circle2.x < 0 || circle2.x > width || circle2.y < 0 || circle2.y > height){
+  function checkOffscreen(){
+  if (circle1.x < 0 || circle1.x > width || circle1.y < 0 || circle1.y > height || circle2.x < 0 || circle2.x > width || circle2.y < 0 || circle2.y > height){
     return true;
   }
   else {
     false;
   }
   // returns "sad ending"
+}
 
   // check if circles overlap
+  function checkOverlap(){
   let d = dist(circle1.x, circle1.y, circle2.x, circle2.y);
-  if d < circle1.size/2 + circle2.size/2 {
+  if (d < circle1.size/2 + circle2.size/2) {
   // returns "true love"
   }
+}
 
-
-  // displays the circles
+  function display(){
   fill(255, 0, 0);
   ellipse(circle1.x, circle1.y, circle1.size);
   ellipse(circle2.x, circle2.y, circle2.size);
-
-
 }
