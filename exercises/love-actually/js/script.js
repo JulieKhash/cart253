a71b02076c76921fd41aadba99ad42966d143a09
Julie <gulshan.khashimova@gmail.com>
2021-10-03 18:41:02 -0400

    E5: made the users circle move

--- exercises/love-actually/js/script.js
+++ exercises/love-actually/js/script.js
@@ -4,7 +4,7 @@ let circleA = {
   size: 100,
   vx: 0,
   vy: 0,
-  speed: 0
+  speed: 3
 }
 let circleB = {
   x: undefined,
@@ -12,7 +12,7 @@ let circleB = {
   size: 100,
   vx: 0,
   vy: 0,
-  speed: 0
+  speed: 2
 }
 let circleC = {
   x: undefined,
@@ -20,7 +20,7 @@ let circleC = {
   size: 100,
   vx: 0,
   vy: 0,
-  speed: 0
+  speed: 3
 }
 
 
@@ -28,17 +28,45 @@ let circleC = {
 function setup(){
   createCanvas(windowWidth, windowHeight);
 
+  circleB.x = width/3;
+  circleB.y = height/3;
 
+
+  circleB.vx = circleB.vx + circleB.speed;
+  circleB.vy = circleB.vy + circleB.speed;
 }
 
 
 function draw(){
   background(0);
 
+  // control user's circle with mouseX and mouseY
+  if (mouseX < circleB.x) {
+    circleB.vx = -circleB.speed;
+  }
+  else {
+    circleB.vx = circleB.speed;
+  }
+
+  if (mouseY < circleB.y) {
+    circleB.vy = -circleB.speed;
+  }
+  else {
+    circleB.vy = circleB.speed;
+  }
+
+
+
+  //moves the user's circle
+  circleB.x = circleB.x + circleB.vx;
+  circleB.y = circleB.y + circleB.vy;
+
+
+  //display circles
   fill(255, 0, 0);
   ellipse(circleA.x, circleA.y, circleA.size);
   fill(0, 255, 0);
-  ellipse(mouseX, mouseY, circleB.size);
+  ellipse(circleB.x, circleB.y, circleB.size);
   fill(0, 0, 255);
-  ellipse(circle.x, circle.y, circle.size);
+  ellipse(circleC.x, circleC.y, circleC.size);
 }
