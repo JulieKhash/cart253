ea24c708d06ee7adea1fb872263415812df8fc6a
Julie <gulshan.khashimova@gmail.com>
2021-10-04 23:26:43 -0400

    E5: made the circles move in  random directions

--- exercises/love-activity/js/script.js
+++ exercises/love-activity/js/script.js
@@ -5,6 +5,8 @@ Author Name
 This is a template. You must fill in the title,
 author, and this description to match your project!
 */
+
+///press a key to get started
 let chessboarImg;
 
 let circle1 ={
@@ -40,20 +42,34 @@ chessboarImg = loadImage("assets/images/chessboard.png")
 function setup() {
   createCanvas(700, 700);
 
-  circle1.x = width-600;
-  circle1.y = height-600;
+  circle1.x = width-630;
+  circle1.y = height-630;
 
-  circle2.x = width -100;
-  circle2.y = height-100;
+  circle2.x = width -70;
+  circle2.y = height-70;
+
+  //start circles moving
+  circle1.vx = random(-circle1.speed, circle1.speed);
+  circle1.vy = random(-circle1.speed, circle1.speed);
+
+  circle2.vx = random(-circle2.speed, circle2.speed);
+  circle2.vy = random(-circle2.speed, circle2.speed);
 
 }
-
 
 
 function draw() {
   background(0);
   imageMode(CENTER);
   image(chessboarImg, width/2, height/2, 650, 650);
+
+
+  //makes the circles move
+  circle1.x = circle1.x + circle1.vx;
+  circle1.y = circle1.y + circle1.vy;
+
+  circle2.x = circle2.x + circle2.vx;
+  circle2.y = circle2.y + circle2.vy;
 
 
   fill(255, 0, 30);
