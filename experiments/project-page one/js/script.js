c76da6db62381cf2bb411fa786b40f65b4bc0119
Julie <gulshan.khashimova@gmail.com>
2021-10-06 22:29:37 -0400

    working on animation

--- experiments/project-page one/js/script.js
+++ experiments/project-page one/js/script.js
@@ -11,6 +11,37 @@ let handPosition ={
   alpha:100
 }
 
+let circle1 = {
+  x: undefined,
+  y: undefined,
+  size:90,
+  speed:2,
+  vx:0,
+  vy:0,
+  alpha: 200,
+}
+
+let circle2 = {
+  x: undefined,
+  y: undefined,
+  size: 40,
+  speed:1.5,
+  vx:0,
+  vy:0,
+  alpha: 200,
+}
+
+let circle3 = {
+  x: undefined,
+  y: undefined,
+  size:110,
+  speed:2,
+  vx:0,
+  vy:0,
+  alpha: 190,
+}
+
+
 
 
 function preload() {
@@ -23,11 +54,16 @@ function preload() {
 function setup() {
   createCanvas(1900, 1300);
 
+
+
 }
 
 function draw() {
-  // x = width/3+190;
-  // y = height/2;
+
+//isNoloop();
+//CallAnimation();
+//handAnimation();
+//isHandonHand();
 
 background(0);
 imageMode(CENTER);
@@ -36,10 +72,58 @@ image(shadowImg, width/2, height/2, 800, 590);
 tint(255, 100); // how to apply a tint to a single code?
 image(handImg, mouseX, mouseY, 140, 190);
 
-
 //hand postion point
 noStroke();
 fill(200, handPosition.alpha);
 ellipse(handPosition.x, handPosition.y, handPosition.place);
-
 }
+//touch animation
+// function CallAnimation(){
+//   if (isHandonHand()){
+//   handAnimation();
+//   }
+// }
+
+    //function handAnimation(){
+     // let d = dist(mouseX, mouseY, handPosition.x, handPosition.y);
+     // if (d < handPosition.place/5){
+     //if (mouseX > handPosition.place/5){
+    noStroke();
+    circle1.size += 1;
+    circle1.size = constrain(circle1.size, 20, 210);
+    fill(39, 169, 179, circle1.alpha);
+    circle1.alpha -= circle1.speed;
+    ellipse(handPosition.x, handPosition.y, circle1.size)
+
+    circle2.size += 0.9;
+    circle2.size = constrain(circle2.size, 30, 220);
+    fill(39, 180, 179, circle2.alpha);
+    circle2.alpha -= circle2.speed;
+    ellipse(handPosition.x, handPosition.y, circle2.size);
+
+    circle3.size += 1.2;
+    circle3.size = constrain(circle3.size, 30, 220);
+    fill(39, 169, 179, circle3.alpha);
+    circle3.alpha -= circle3.speed;
+    ellipse(handPosition.x, handPosition.y, circle3.size);
+  
+
+
+
+//check if it's not looping
+// function isNoloop(){
+//   if (isHandonHand()) {
+//     noLoop();
+//   }
+// }
+
+//checks if hand is on hand
+ // function isHandonHand(){
+ //   let d = dist(mouseX, mouseY, handPosition.x, handPosition.y);
+ //   if (d < handPosition.place/5){
+ //     return true;
+ //   }
+ //  else {
+ //    return false;
+ //  }
+ // }
