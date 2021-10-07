32e7efd961104eb81e632743068162913531d602
Julie <gulshan.khashimova@gmail.com>
2021-10-06 21:48:54 -0400

    started a new page for project

--- experiments/project-page one/js/script.js
+++ experiments/project-page one/js/script.js
@@ -1,33 +1,45 @@
-/**
-Title of Project
-Author Name
-
-This is a template. You must fill in the title,
-author, and this description to match your project!
-*/
 
 "use strict";
 
+let handImg;
+let shadowImg;
 
-/**
-Description of preload
-*/
+let handPosition ={
+  x: 810,
+  y: 665,
+  place: 90,
+  alpha:100
+}
+
+
+
 function preload() {
-
+  shadowImg = loadImage("assets/images/shadow.gif");
+  handImg =  loadImage("assets/images/handgre.png");
 }
 
 
-/**
-Description of setup
-*/
+
 function setup() {
+  createCanvas(1900, 1300);
 
 }
 
-
-/**
-Description of draw()
-*/
 function draw() {
+  // x = width/3+190;
+  // y = height/2;
+
+background(0);
+imageMode(CENTER);
+tint(100, 210, 210);
+image(shadowImg, width/2, height/2, 800, 590);
+tint(255, 100); // how to apply a tint to a single code?
+image(handImg, mouseX, mouseY, 140, 190);
+
+
+//hand postion point
+noStroke();
+fill(200, handPosition.alpha);
+ellipse(handPosition.x, handPosition.y, handPosition.place);
 
 }
