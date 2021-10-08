fce3f08ca76077786e7e17c280e563bcc78c4c76
Julie <gulshan.khashimova@gmail.com>
2021-10-07 23:07:16 -0400

    created a text box

--- experiments/project-page-three/js/script.js
+++ experiments/project-page-three/js/script.js
@@ -3,6 +3,7 @@
 
 let fireImg;
 let burnImg;
+let currentInput = ``;
 
 function preload() {
   fireImg = loadImage("assets/images/fire.gif");
@@ -19,7 +20,7 @@ function setup() {
 
 function draw() {
   let glitter = random(0, 40); // make bg orangy color
-  background(100);
+  background(0);
 
 
   imageMode(CENTER);
@@ -30,9 +31,39 @@ function draw() {
 
 
   let col = random(0, 220);
+  noStroke();
   textFont(`Verdana`);
   textSize(20);
   fill(col);
   textAlign(CENTER, CENTER);
   text(`What is it you wish to burn away?`, width/2, height/3+400);
+
+  //Diplay Input
+  stroke(184, 75, 48);
+  fill(glitter, 100); //sets opacity at half
+  rectMode(CENTER);
+  rect(width/3, height/4, 500, 100)
+
+  noStroke();
+  textSize(40);
+  fill(col, 75, 48);
+  text(currentInput, width/3, height/4);
+}
+
+//add the typed key to the input string
+function keyTyped(){
+
+  let numKeys = 5;
+// lomit the number of key input
+  for (let i = 0; i < numKeys; i++);
+
+  currentInput += key;
+
+
+}
+
+function keyPressed(){
+  if (keyCode === BACKSPACE){
+    currentInput = ``;
+  }
 }
