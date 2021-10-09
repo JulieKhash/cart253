08f3950294d1222f8aa6f26ad522ee62dc3be664
Julie <gulshan.khashimova@gmail.com>
2021-10-09 00:42:37 -0400

    almost done with final page

--- experiments/project-page-five/js/script.js
+++ experiments/project-page-five/js/script.js
@@ -15,10 +15,10 @@ let light = {
 }
 
 let handCatcher ={
-  x: 950,
-  y: 1000,
-  sizeX: 200,
-  sizeY: 272,
+  x: 650,
+  y: 1100,
+  sizeX: 170,
+  sizeY: 231,
   vx: 0,
   vy: 0,
   speed: 2.5
@@ -29,7 +29,7 @@ let yoff = 0;
 
 function preload() {
   lightImg = loadImage("assets/images/light.png");
-  handCatcherImg =  loadImage("assets/images/handbrighter.png");
+  handCatcherImg =  loadImage("assets/images/handtransparent.png");
   faceImg = loadImage("assets/images/face.png");
 }
 
@@ -49,8 +49,8 @@ function draw() {
   light.x = light.x + light.vx;
   light.y = light.y + light.vy;
 
-  light.x = constrain(light.x, 0, width);
-  light.y = constrain(light.y, 0, height);
+  light.x = constrain(light.x, 200, width-200);
+  light.y = constrain(light.y, 200, height);
 
 
   handCatcher.vx += handCatcher.speed;
@@ -85,6 +85,8 @@ function draw() {
     light.size = constrain(light.size, 300, 6000);
     fill(0, 0, 0, 100);
     image(faceImg, light.x, light.y);
+
+
   }
 
 
@@ -92,13 +94,17 @@ function draw() {
   handCatcher.y += handCatcher.vy;
 
 
-
-
   imageMode(CENTER);
   image(lightImg, light.x, light.y, light.size, light.size);
 
 
   image(handCatcherImg, handCatcher.x, handCatcher.y, handCatcher.sizeX, handCatcher.sizeY);
+
+  let glitch = random(0, 255);
+  textFont(`Verdana`);
+  textSize(25);
+  fill(glitch);
+  text(`Catch the Light`, width/2-220, height/2+10);
 
 
 
