cc00fbc9fd9441b5eb66a3549e1571c0a90bac92
Julie <gulshan.khashimova@gmail.com>
2021-10-09 16:09:51 -0400

    worked on text

--- experiments/project-page-two/js/script.js
+++ experiments/project-page-two/js/script.js
@@ -16,30 +16,29 @@ let mousePointer ={
   size: 100
 }
 
-let circle1 = {
+// Focus Animation ellipses
+let ellipse1 = {
   x: 950,
   y: 650,
-
   size: 50,
   speed: 9,
   strokeSize: 10
 }
-let circle2 = {
+let ellipse2 = {
   x: 950,
   y: 650,
   size: 40,
   speed: 8,
  strokeSize: 10
 }
-let circle3 = {
+let ellipse3 = {
   x: 950,
   y: 650,
-
   size: 30,
   speed: 7,
  strokeSize: 10
 }
-let circle4 = {
+let ellipse4 = {
   x: 950,
   y: 650,
 
@@ -47,37 +46,29 @@ let circle4 = {
   speed: 6,
  strokeSize: 10
 }
-let circle5 = {
+let ellipse5 = {
   x: 950,
   y: 650,
-
   size: 10,
   speed: 4,
  strokeSize: 10
 }
-let circle6 = {
+let ellipse6 = {
   x: 950,
   y: 650,
-
   size: 5,
   speed: 3,
   strokeSize: 10
 }
 
-
-
-
 function preload(){
   focusImg = loadImage("assets/images/focus.gif");
 }
 
-
 function setup(){
   createCanvas(1900, 1300);
 
-
 }
-
 
 
 function draw(){
@@ -85,27 +76,25 @@ function draw(){
   let bg = random (0, 23);
   background(bg);
 
-
-
-
-
-  //circle aim position
+  //aim position
   stroke(0);
   noFill();
   ellipse(aimPosition.x, aimPosition.y, aimPosition.place);
 
 
+  eyeImage();
+  controlImage();
+  showFocus();
+  displayAim();
+  text4()
+  showClicktext()
 
+  //displays eye image at the center
+  function eyeImage(){
   imageMode(CENTER);
   tint(255, 255);
   image(focusImg, width/2, height/2,600, 450);
-  let dx = mouseX - focusImg.width/2 - offset;
-  let dy = mouseY - focusImg.height/2 - offset2;
-  offset += dx * easing;
-  offset2 += dy * easing;
-  tint(255, 150);
-  image(focusImg, offset, offset2, 600, 450);
-  //loopAnimation()
+
 
   //pointer
   stroke(255, 0);
@@ -113,97 +102,131 @@ function draw(){
   line(offset, offset2+30, offset, offset2-30);
   stroke(255,0);
   ellipse(offset, offset2, mousePointer.size); //mouse pointer
+}
 
-  //check the distances
-  let d = dist(offset, offset2, aimPosition.x, aimPosition.y);
-  if (d < aimPosition.place/20){
-  loopAnimation();
+  //controls second image with mouse
+  function controlImage(){
+  let dx = mouseX - focusImg.width/2 - offset;
+  let dy = mouseY - focusImg.height/2 - offset2;
+  offset += dx * easing;
+  offset2 += dy * easing;
+  tint(255, 150); //
+  image(focusImg, offset, offset2, 600, 450);
+}
+}
 
+  //shows focus animation if the aimed
+  function showFocus(){
+    if (checkAim()){
+        focusAnimation();
+    }
   }
 
-  //red aim pos
+  //check the distances
+  function checkAim(){
+  let d = dist(offset, offset2, aimPosition.x, aimPosition.y);
+  if (d < aimPosition.place/20){
+    return true;
+  }
+  else {
+    return false
+  }
+}
+
+  //red aim position
+  function displayAim(){
   stroke(255, 0, 0);
   strokeWeight(2);
   line(aimPosition.x-30, aimPosition.y, aimPosition.x+30,aimPosition.y);
   line(aimPosition.x, aimPosition.y+30, aimPosition.x, aimPosition.y-30);
+}
 
+function text4(){
+  let col = random(0, 220);
   noStroke();
   textFont(`Verdana`);
   textSize(40);
   fill(col);
   textAlign(CENTER, CENTER);
   text(`Focus your Mind`, width/2+200, height/2+300);
-
-
 }
 
-function loopAnimation(){
+function focusAnimation(){
   strokeGlitch = random(0,255);
  //1
- circle1.size += circle1.speed;
- circle1.size = constrain(circle1.size, 50, width+150);
- circle1.strokeSize -= 0.05;
- circle1.strokeSize = constrain(circle1.strokeSize, 0.03, 10);
-
+ ellipse1.size += ellipse1.speed;
+ ellipse1.size = constrain(ellipse1.size, 50, width+150);
+ ellipse1.strokeSize -= 0.05;
+ ellipse1.strokeSize = constrain(ellipse1.strokeSize, 0.03, 10);
  stroke(strokeGlitch);
- strokeWeight(circle1.strokeSize);
+ strokeWeight(ellipse1.strokeSize);
  noFill();
- ellipse(circle1.x, circle1.y, circle1.size);
-
+ ellipse(ellipse1.x, ellipse1.y, ellipse1.size);
  //2
- circle2.size += circle2.speed;
- circle2.size = constrain(circle2.size, 20, width+100);
- circle2.strokeSize -= 0.05;
- circle2.strokeSize = constrain(circle2.strokeSize, 0.04, 10);
-
+ ellipse2.size += ellipse2.speed;
+ ellipse2.size = constrain(ellipse2.size, 20, width+100);
+ ellipse2.strokeSize -= 0.05;
+ ellipse2.strokeSize = constrain(ellipse2.strokeSize, 0.04, 10);
  stroke(strokeGlitch);
- strokeWeight(circle2.strokeSize);
+ strokeWeight(ellipse2.strokeSize);
  noFill();
- ellipse(circle2.x, circle2.y, circle2.size);
+ ellipse(ellipse2.x, ellipse2.y, ellipse2.size);
  //3
- circle3.size += circle3.speed;
- circle3.size = constrain(circle3.size, 10, width);
- circle3.strokeSize -= 0.04;
- circle3.strokeSize = constrain(circle3.strokeSize, 0.05, 10);
-
+ ellipse3.size += ellipse3.speed;
+ ellipse3.size = constrain(ellipse3.size, 10, width);
+ ellipse3.strokeSize -= 0.04;
+ ellipse3.strokeSize = constrain(ellipse3.strokeSize, 0.05, 10);
  stroke(strokeGlitch);
- strokeWeight(circle3.strokeSize);
+ strokeWeight(ellipse3.strokeSize);
  noFill();
- ellipse(circle3.x, circle3.y, circle3.size);
+ ellipse(ellipse3.x, ellipse3.y, ellipse3.size);
  //4
- circle4.size += circle4.speed;
- circle4.size = constrain(circle4.size, 10, width-50);
- circle4.strokeSize -= 0.03;
- circle4.strokeSize = constrain(circle4.strokeSize, 0.03, 10);
-
+ ellipse4.size += ellipse4.speed;
+ ellipse4.size = constrain(ellipse4.size, 10, width-50);
+ ellipse4.strokeSize -= 0.03;
+ ellipse4.strokeSize = constrain(ellipse4.strokeSize, 0.03, 10);
  stroke(strokeGlitch);
- strokeWeight(circle4.strokeSize);
+ strokeWeight(ellipse4.strokeSize);
  noFill();
- ellipse(circle4.x, circle4.y, circle4.size);
+ ellipse(ellipse4.x, ellipse4.y, ellipse4.size);
 //5
- circle5.size += circle5.speed;
- circle5.size = constrain(circle5.size, 5, 600);
- circle5.strokeSize -= 0.02;
- circle5.strokeSize = constrain(circle5.strokeSize, 2, 10);
-
+ ellipse5.size += ellipse5.speed;
+ ellipse5.size = constrain(ellipse5.size, 5, 600);
+ ellipse5.strokeSize -= 0.02;
+ ellipse5.strokeSize = constrain(ellipse5.strokeSize, 2, 10);
  stroke(strokeGlitch);
- strokeWeight(circle5.strokeSize);
+ strokeWeight(ellipse5.strokeSize);
  noFill();
- ellipse(circle5.x, circle5.y, circle5.size);
-
- //5
-  circle6.size += circle6.speed;
-  circle6.size = constrain(circle5.size, 5, 300);
-  //circle6.strokeSize -= 0.01;
-  circle6.strokeSize = constrain(circle6.strokeSize, 0.03, 9);
-
-  stroke(strokeGlitch);
-  strokeWeight(circle6.strokeSize);
-  noFill();
-  ellipse(circle6.x, circle6.y, circle6.size);
-
+ ellipse(ellipse5.x, ellipse5.y, ellipse5.size);
+ //6
+ellipse6.size += ellipse6.speed;
+ellipse6.size = constrain(ellipse6.size, 5, 300);
+//ellipse6.strokeSize -= 0.01;
+ellipse6.strokeSize = constrain(ellipse6.strokeSize, 0.03, 9);
+stroke(strokeGlitch);
+strokeWeight(ellipse6.strokeSize);
+noFill();
+ellipse(ellipse6.x, ellipse6.y, ellipse6.size);
 
 }
+
+function showClicktext(){
+  if (ellipse6.size === 300 && checkAim()) {
+    text5();
+  }
+}
+
+
+function text5(){
+  let col = random(0, 220);
+  noStroke();
+  textFont(`Verdana`);
+  textSize(25);
+  fill(col);
+  textAlign(CENTER, CENTER);
+  text(`click`, ellipse6.x, ellipse6.y-60);
+}
+
 
 //
 //if (circle6.size === 300){
