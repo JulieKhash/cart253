5ac207c7fbe067c74ae7b33a35d7bc6b400b2137
Julie <gulshan.khashimova@gmail.com>
2021-10-08 20:55:09 -0400

    P1: imported title page and refactored into functions

--- project1/js/script.js
+++ project1/js/script.js
@@ -1,6 +1,6 @@
 /**
-Title of Project
-Author Name
+Project 1 - Secret Signs
+Julie Khashimova
 
 This is a template. You must fill in the title,
 author, and this description to match your project!
@@ -8,26 +8,65 @@ author, and this description to match your project!
 
 "use strict";
 
+let backgroundImg;
+let crescentImg;
 
-/**
-Description of preload
-*/
+let state = `title`
+
 function preload() {
-
+crescentImg = loadImage("assets/images/moon.gif");
+backgroundImg = loadImage("assets/images/headshadow.gif");
 }
 
 
-/**
-Description of setup
-*/
+
 function setup() {
-
+createCanvas(1900, 1300);
 }
 
 
-/**
-Description of draw()
-*/
 function draw() {
+  let col = random(0, 220);
+  background(0);
 
+  titlePage();
+
+
+
+}
+//Dispays first page of the program
+function titlePage(){
+  text2();
+  titleText()
+  titleImages();
+}
+
+  function titleText(){
+  let col = random(0, 220);
+  textFont(`Verdana`);
+  textSize(70);
+  fill(col);
+  textAlign(CENTER, CENTER);
+  text(`SECRET SIGNS`, width/2, height/4.5);
+}
+
+  function text2() {
+  let col = random(0, 220);
+  textFont(`Verdana`);
+  textSize(20);
+  fill(col);
+  textAlign(CENTER, CENTER);
+  text(`Press any key`, width/2, height/3+400);
+
+}
+
+//displays title images
+function titleImages(){
+  let col = random(0, 220);
+
+  imageMode(CENTER);
+  tint(col, 200, 200, 150);
+
+  image(backgroundImg, width/2, height/2);
+  image(crescentImg, width/2, height/3+210, 500, 276);
 }
