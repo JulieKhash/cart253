7dc9ac20d87261b19a14bab1c2f1b3e6b617168d
Julie <gulshan.khashimova@gmail.com>
2021-10-09 23:07:11 -0400

    Working on textfield and aesthetics

--- experiments/project-page-three/js/script.js
+++ experiments/project-page-three/js/script.js
@@ -3,21 +3,35 @@
 
 let fireImg;
 let burnImg;
-let fireeImg
+let ashesImg;
 
 let currentInput = ``;
-let numStars = 700
+let numAshes = 700
+let numKeysTyped = 0;
+
+let button = {
+  size: 100
+}
+
+let textfield = {
+  x: 550,
+  y: 360,
+  w: 600,
+  h: 200
+}
+
+
 
 function preload() {
   fireImg = loadImage("assets/images/fire.gif");
-  fireeImg = loadImage("assets/images/burn.gif");
-  burnImg = loadImage("assets/images/ashes-dust.gif");
+  burnImg = loadImage("assets/images/burn.gif");
+  ashesImg = loadImage("assets/images/ashes-dust.gif");
 }
 
 
 function setup() {
   createCanvas(1900, 1300);
-  createInput();
+  textfield = createInput();
 
 }
 
@@ -29,16 +43,14 @@ function draw() {
 
 
 
-  imageMode(CENTER);
-  tint(255, 255);
-  image(fireImg, width/2+150, height/2, 500*2, 700*2);
-  // tint(255,50);
-  // image(fireeImg, width/2+200, height/2, 600*2, 1017*2);
-  // tint(220, 30, 0, 50);
+  //textbox();
+  displayFireImage1()
+  text7()
+  text6();
+  userTextinput();
+  makeButton();
 
-
-
-  for (let i = 0; i < numStars; i++){
+  for (let i = 0; i < numAshes; i++){
   let x = random(0, width);
   let y = random(0, height);
   let ashSize = 3
@@ -48,43 +60,83 @@ function draw() {
   strokeWeight(2);
   ellipse(x, y, ashSize);
   }
+}
 
-
+function text7(){
   let col = random(0, 220);
   noStroke();
   textFont(`Verdana`);
   textSize(20);
   fill(col);
+  // if (userTextinput()){
+  // fill(0,0);
+  // }
   textAlign(CENTER, CENTER);
   text(`What is it you wish to burn away?`, width/2, height/3+400);
-
-  //Diplay Input Field
-  //stroke(184, 75, 48);
-  fill(glitter, 100); //sets opacity at half
-  rectMode(CENTER);
-  rect(width/3, height/4, 600, 200)
-
-  // noStroke();
-  // textSize(20);
-  // fill(col);
-  // text(`What is it you wish to burn away?`, width/3, height/4);
-
-  noStroke();
-  textSize(30);
-  fill(col);
-  text(`write here`, width/3, height/4);
-
-  //noStroke();
-  textSize(40);
-  fill(col, 75, 48);
-  text(currentInput, width/3, height/4);
 }
 
 
 
+function textbox(){
+  //Diplay Input Field
+  let glitter = random(0, 40); // make bg orangy color
+  fill(glitter, 100); //sets opacity at half
+  rectMode(CENTER);
+  rect(width/3-150, height/4, 650, 250);
+}
 
+//displays first fire image
+function displayFireImage1(){
+imageMode(CENTER);
+image(fireImg, width/2+200, height/2, 500*2-200, 700*2-200);
+}
+
+//displays textfield text
+  function text6(){
+  let col = random(0, 220);
+  noStroke();
+  textSize(30);
+  fill(col);
+  if (numKeysTyped >= 1){
+  fill(0,0);
+}
+  text(`write here`, width/3+ 30, height/4);
+}
+
+//displays user input text
+  function userTextinput(){
+  let col = random(0, 220);
+  textSize(90);
+  fill(col, 75, 48);
+  text(currentInput, width/3+70, height/4);
+}
+
+//checks and limits the number of character input
 function keyTyped() {
+  numKeysTyped = numKeysTyped + 1;
+  if (numKeysTyped < 30){
   currentInput += key;
+}
+}
+
+//displays a button
+function makeButton(){
+  if (numKeysTyped > 3){
+    let glitter = random(0, 30);
+    fill(255, 0, 0, glitter);
+    ellipse(width/2, height/2-200, 100);
+    fill(150, 0, 0);
+    textSize(25);
+    text(`burn`, width/2, height/2-200)
+
+    tint(200, 100, 0, 50);
+    image(ashesImg, width/2+200, height/2, 500*5, 300*5);
+    //tint(100,50);
+    //image(burnImg, width/2+200, height-300, 190*5, 355*5);
+    tint(0,0);
+    image(fireImg, width/2+200, height/2, 500*2-200, 700*2-200);
+
+  }
 }
 
 
@@ -93,7 +145,10 @@ function keyPressed(){
     currentInput = ``;
   }
   else if (keyCode === ENTER){
-    keyCode === SPACEBAR;
 
   }
 }
+
+// function mousePressed(){
+//   if()
+// }
