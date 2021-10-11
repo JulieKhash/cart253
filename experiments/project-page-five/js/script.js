c010663954cb62558f939289c47439d14f7067c6
Julie <gulshan.khashimova@gmail.com>
2021-10-11 16:59:44 -0400

    P1: finished final page

--- experiments/project-page-five/js/script.js
+++ experiments/project-page-five/js/script.js
@@ -44,22 +44,21 @@ function setup() {
 function draw() {
   let glitter= random(0,25);
   background(glitter);
-
-  light.vx =  random(-light.speed, light.speed);
-  light.vy =  random(-light.speed, light.speed);
-  //
-  light.x = light.x + light.vx;
-  light.y = light.y + light.vy;
-
-  light.x = constrain(light.x, 200, width-200);
-  light.y = constrain(light.y, 200, height);
+  imageMode(CENTER);
 
 
-  handCatcher.vx += handCatcher.speed;
-  handCatcher.vy += handCatcher.speed;
+  showFace();
+  displayHand();
+  text10();
+  moveLight();
+  displayLight();
 
+  controlHand();
+  bird();
 
+}
 
+ function controlHand(){
   //control user's vertical movement
   if (keyIsDown (LEFT_ARROW)) {
     handCatcher.vx = -handCatcher.speed;
@@ -80,42 +79,89 @@ function draw() {
   else {
     handCatcher.vy = 0;
   }
+  handCatcher.x += handCatcher.vx;
+  handCatcher.y += handCatcher.vy;
+}
 
+
+ function isCaught(){
   let d = dist(handCatcher.x, handCatcher.y, light.x, light.y);
   if (d < handCatcher.sizeY/5 + light.size/5){
+    return true;
+  }
+  else {
+    return false;
+  }
+}
+
+
+  function showFace(){
+  if (isCaught()){
     light.size +=light.speed;
     light.size = constrain(light.size, 300, 6000);
     fill(0, 0, 0, 100);
     image(faceImg, light.x, light.y);
-
-
+}
   }
 
 
-  handCatcher.x += handCatcher.vx;
-  handCatcher.y += handCatcher.vy;
+
+   function moveLight(){
+   light.vx =  random(-light.speed, light.speed);
+   light.vy =  random(-light.speed, light.speed);
+   //
+   light.x = light.x + light.vx;
+   light.y = light.y + light.vy;
+
+   light.x = constrain(light.x, 200, width-200);
+   light.y = constrain(light.y, 200, height);
+  }
 
 
-  imageMode(CENTER);
+
+  function displayLight(){
   image(lightImg, light.x, light.y, light.size, light.size);
+}
 
-  if (light.size === 6000){
-    image(birdsImg, light.x, light.y);
-  }
 
+ function displayHand(){
+
+  handCatcher.vx += handCatcher.speed;
+  handCatcher.vy += handCatcher.speed;
 
   image(handCatcherImg, handCatcher.x, handCatcher.y, handCatcher.sizeX, handCatcher.sizeY);
+};
 
+
+// displasy bird and the farewell text;
+ function bird(){
+  if (light.size === 6000){
+    image(birdsImg, light.x, light.y);
+    text11()
+  }
+}
+
+function text10(){
+let glitch = random(0, 255);
+textFont(`Verdana`);
+textSize(25);
+fill(glitch);
+if (isCaught()){
+  fill(0,0);
+}
+text(`Catch the Light`, width/2+100, height/2+10);
+}
+
+
+
+  //
+  function text11(){
   let glitch = random(0, 255);
   textFont(`Verdana`);
   textSize(25);
   fill(glitch);
-  //text(`Catch the Light`, width/2-220, height/2+10);
-
   text(`We will meet again`, width/2-220, height/2+10);
-
-
-
-
 }
+
+
   // check if user catcher the light
