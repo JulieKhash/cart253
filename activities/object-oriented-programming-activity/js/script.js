89b87423dab30cccbdb88e32ffefbc8312fd3777
Julie <gulshan.khashimova@gmail.com>
2021-10-24 14:17:33 -0400

    A: added the paddle into the main script

--- activities/object-oriented-programming-activity/js/script.js
+++ activities/object-oriented-programming-activity/js/script.js
@@ -1,14 +1,20 @@
 
 "use strict";
 
-
+let paddle;
 
 
 function setup() {
+  createCanvas(800, 800);
+  paddle = new Paddle(200, 40);
 
 }
 
 
 function draw() {
+  background(0);
+
+  paddle.move();
+  paddle.display();
 
 }
