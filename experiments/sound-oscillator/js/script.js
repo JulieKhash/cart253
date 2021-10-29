be19de6ce722f3b39c0911ae2fd7afa80acadf42
Julie <gulshan.khashimova@gmail.com>
2021-10-29 12:32:46 -0400

    Worked on osc sounds

--- experiments/sound-oscillator/js/script.js
+++ experiments/sound-oscillator/js/script.js
@@ -1,33 +1,50 @@
-/**
-Title of Project
-Author Name
-
-This is a template. You must fill in the title,
-author, and this description to match your project!
-*/
-
 "use strict";
 
+let oscillator;
+//let angle = 0;
+let t = 0;
 
-/**
-Description of preload
-*/
-function preload() {
-
-}
-
-
-/**
-Description of setup
-*/
 function setup() {
+  createCanvas(600, 600);
+  userStartAudio();
 
+  oscillator = new p5.Oscillator(440, `sine`);
+  oscillator.amp(0.1);
 }
 
-
-/**
-Description of draw()
-*/
 function draw() {
+  background(0);
 
+  // let newFreq = map(mouseY, height, 0, 20, 20000);
+
+  // let sinAngle = tan(angle);
+  // let newFreq = map(sinAngle, -1, 1, 0, 880);
+
+  // let r = random(0,1);
+  // let newFreq = map(r, 0, 1, 440, 880);
+
+  let noiseValue = noise(t);
+  let newFreq = map(noiseValue, 0, 1, 440, 2800);
+  oscillator.freq(newFreq);
+
+  t = t + 0.1;
+  // angle = angle + 0.2;
+
+  push();
+  textSize(30);
+  textAlign(LEFT, CENTER);
+  fill(255, 0, 0);
+  text(newFreq, 100, height / 2);
+  pop();
+
+  // let newAmp = map(mouseX, 0, width, 0, 0.5);
+  // oscillator.amp(newAmp);
+}
+
+function mousePressed() {
+  oscillator.start();
+}
+
+function mouseReleased() {
+  oscillator.stop();
 }
