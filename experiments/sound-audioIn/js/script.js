ff8270c83fc673da4bd68265051274b89045002b
Julie <gulshan.khashimova@gmail.com>
2021-10-29 16:02:07 -0400

    Finished ghost+mic

--- experiments/sound-audioIn/js/script.js
+++ experiments/sound-audioIn/js/script.js
@@ -1,40 +1,56 @@
 "use strict";
 
-let synth;
-let notes = [`Fb2`, `G2`, `Ab4`, `Db5`, `Eb4`, `Fb4`];
-let currentNote = 0;
+let mic; // variabke to store the object
+let ghost = {
+  x: 0,
+  y: 0,
+  vx: 0,
+  vy: 0,
+  image: undefined,
+};
 
-
+function preload() {
+  ghost.image = loadImage(`assets/images/clown.png`);
+}
 
 function setup() {
   createCanvas(600, 600);
 
-  synth = new p5.PolySynth();
+  ghost.x = width / 2;
+  ghost.y = height / 2;
 
-  userStartAudio();
-
+  mic = new p5.AudioIn();
+  mic.start();
 }
 
 function draw() {
   background(0);
 
-}
+  // get volume into microphone
+  let level = mic.getLevel();
 
-function keyPressed(){
+  ghost.x += random(-1, 1);
+  ghost.y += random(-1, 1);
 
-//start random ghost player
-setInterval(playRandomNote, 500);
-}
-
-function playRandomNote(){
-  // let randomNote = random(notes);
-  // synth.play(randomNote, 1, 0, 0.3);
-
-  let note = notes[currentNote];    //play a note in order
-  synth.play(note, 1, 0, 0.3);
-
-  currentNote += 1;
-  if (currentNote === notes.length){
-    currentNote = 0;
+  // check if the ghost is scared
+  if (level > 0.1) {
+    // runs to the right
+    ghost.vx = 20;
   }
+
+  ghost.x += ghost.vx;
+  ghost.y += ghost.vy;
+
+  // console.log(level);
+  // let size = map(level, 0, 1, 0, width);
+  //
+  // push();
+  // fill(255, 0, 0);
+  // ellipse(width/2, height/2, size);
+  // pop();
+  push();
+  imageMode(CENTER);
+  tint(255, 50);
+  image(ghost.image, ghost.x, ghost.y);
+  pop();
 }
--- experiments/sound-polySynth/js/script.js
+++ experiments/sound-polySynth/js/script.js
@@ -4,37 +4,32 @@ let synth;
 let notes = [`Fb2`, `G2`, `Ab4`, `Db5`, `Eb4`, `Fb4`];
 let currentNote = 0;
 
-
-
 function setup() {
   createCanvas(600, 600);
 
   synth = new p5.PolySynth();
 
   userStartAudio();
-
 }
 
 function draw() {
   background(0);
-
 }
 
-function keyPressed(){
-
-//start random ghost player
-setInterval(playRandomNote, 500);
+function keyPressed() {
+  //start random ghost player
+  setInterval(playRandomNote, 500);
 }
 
-function playRandomNote(){
+function playRandomNote() {
   // let randomNote = random(notes);
   // synth.play(randomNote, 1, 0, 0.3);
 
-  let note = notes[currentNote];    //play a note in order
+  let note = notes[currentNote]; //play a note in order
   synth.play(note, 1, 0, 0.3);
 
   currentNote += 1;
-  if (currentNote === notes.length){
+  if (currentNote === notes.length) {
     currentNote = 0;
   }
 }
