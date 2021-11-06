f439073e55ae9b78d5c2b389031b00903de8b8e3
Julie <gulshan.khashimova@gmail.com>
2021-11-06 17:14:50 -0400

    made rects move to different directions

--- experiments/multiple_rect_objects/js/Rectangles.js
+++ experiments/multiple_rect_objects/js/Rectangles.js
@@ -1,9 +1,17 @@
 class Rect {
-  constructor(x, y, size) {
+  constructor(x, y, vx, vy, size) {
     this.x = x;
     this.y = y;
+    this.vx = vx;
+    this.vy = vy;
     this.size = size;
   }
+
+  move(){
+    this.x += this.vx;
+    this.y += this.vy;
+  }
+
 
   display() {
     fill(255, 0, 20);
--- experiments/multiple_rect_objects/js/script.js
+++ experiments/multiple_rect_objects/js/script.js
@@ -4,13 +4,15 @@ let numRect = 4;
 let rects = [];
 
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(400, 400, WEBGL);
 
   for (let i = 0; i < numRect; i++) {
     let x = width / 2;
     let y = height / 2;
-    let size = 100;
-    let rectangle = new Rect(x, y, size);
+    let vx = random(-1,1);
+    let vy = random(-1,1);
+    let size = random(20, 30*i);
+    let rectangle = new Rect(x, y, vx*i, vy*i, size);
     rects.push(rectangle);
   }
 }
@@ -20,6 +22,7 @@ function setup() {
 
     for (let i = 0; i < rects.length; i++) {
     let rectangle = rects[i];
+    rectangle.move();
     rectangle.display();
   }
 }
