ddc4eecb28dac7ec492173f2e6c386f1ff45d8b8
Julie <gulshan.khashimova@gmail.com>
2021-11-06 20:25:45 -0400

    created a cool cube rotation effect

--- experiments/plane_shapes/js/matrix_effect.js
+++ experiments/plane_shapes/js/matrix_effect.js
@@ -6,13 +6,31 @@ let circleColor = 255;
 let circleAlpha = 1;
 let colorChangeRate = 0.01;
 
+
+let music;
+let amp;
+
+let lightImg;
+let sunImg;
+let angelImg;
+
+let numEllipses = 5;
+let size = 300
+
+function preload(){
+  lightImg = loadImage(`assets/images/blue.png`);
+  sunImg = loadImage(`assets/images/light.png`);
+  music = loadSound(`assets/sounds/dream.mp3`);
+}
+
 function setup() {
   createCanvas(1000, 1000, WEBGL);
-  perspective(PI/3, width / height, 0.1, 1000);
+  perspective(PI/3, width / height, 80, 1000);
 }
 
 function draw() {
   background(0);
+  //orbitControl();
 
   for (let i = 0; i < numCircles; i++) {
     // fill(0, circleColor, circleColor, circleAlpha);   //transparency
@@ -23,18 +41,20 @@ function draw() {
     //   circleAlpha = 1;
     // }
     //orbitControl();
-    stroke(0, i * 20, i * 20);
+    //stroke(0, i * 20, i * 20);
     //noStroke();
 
     noFill();
+    noStroke();
     //translate(width/20, height/20);
-    //rotate(i/2 + frameCount/40);
+    rotate(i/2 + frameCount/40);
 
-    //rotateX(frameCount * -0.01);
-    // rotateY(frameCount * -0.01);
-    rotateZ(frameCount * -0.001);
+    //rotateX(frameCount * -0.001);
+    //rotateY(frameCount * -0.001);
+    rotateZ(frameCount * -0.002);
 
     rectMode(CENTER);
+    texture(lightImg);
     translate(-15, 0, sin(frameCount / 30) * 95);
     box(100);
   }
